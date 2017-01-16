6dc359b44fd7ec8024a83b8bf44d48b531883a93
Zivko2 <>
2017-01-16 22:24:55 +0100

    ApexSQL Diff synchronization

---
+++ Database2014/Tables/dbo.Testt.sql
@@ -0,0 +1,14 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+GO
+-- Create table [dbo].[Testt]
+Print 'Create table [dbo].[Testt]'
+GO
+CREATE TABLE [dbo].[Testt] (
+		[broj]      [int] NULL,
+		[nesto]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+GO
+ALTER TABLE [dbo].[Testt] SET (LOCK_ESCALATION = TABLE)
+GO
