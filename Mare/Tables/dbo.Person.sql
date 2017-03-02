09d4398d4f732029498d1dd97ad49427a5242c7a
Zivko2 <>
2017-03-02 15:48:42 +0100

    Prvi commit

---
+++ Mare/Tables/dbo.Person.sql
@@ -0,0 +1,14 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Person] (
+		[id]              [int] NULL,
+		[firstName]       [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[lastName]        [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[isAlive]         [bit] NULL,
+		[age]             [int] NULL,
+		[dateOfBirth]     [binary](8) NULL,
+		[spouse]          [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
