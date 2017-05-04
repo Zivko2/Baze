a95bc5478f629efe259eead2fb74f23f3e1c0d27
ZIvko2 <>
2017-05-04 16:55:43 +0200

    Promena imena tabele

---
+++ ApexSQL/Tables/dbo.AdditionalBarcode5.sql
@@ -0,0 +1,24 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[AdditionalBarcode5] (
+		[Oid]                     [uniqueidentifier] NOT NULL ROWGUIDCOL,
+		[BarCode]                 [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+		[EntityName]              [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+		[EntityID]                [uniqueidentifier] NULL,
+		[OptimisticLockField]     [int] NULL,
+		[GCRecord]                [int] NULL,
+		CONSTRAINT [PK_AdditionalBarcode]
+		PRIMARY KEY
+		CLUSTERED
+		([Oid])
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [iGCRecord_AdditionalBarcode]
+	ON [dbo].[AdditionalBarcode5] ([GCRecord])
+	ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AdditionalBarcode5] SET (LOCK_ESCALATION = TABLE)
+GO
