1f9c6d8fb018226af5c099a7b8bac568e29fb38c
Zivko2 <>
2017-10-04 23:35:50 +0200

    kkkk

---
+++ SC/New/Tables/dbo.test2.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[test2] (
+		[xaxax]      [int] NULL,
+		[asxaxa]     [varchar](22) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[test2] SET (LOCK_ESCALATION = TABLE)
+GO
